375e8d66c7f73d1cc51d3018f7fd2344ba0f141f
Laurent Fréval <laurent.freval@outlook.com>
2019-12-09 09:54:37 +0100

    Ajoutez des fichiers projet.

---
+++ vavavoum/Module1.vb
@@ -0,0 +1,12 @@
+﻿Option Explicit On
+Option Strict On
+
+Module Module1
+
+    Sub Main()
+        Dim maClio As New Voiture()
+        Console.WriteLine(maClio.lesRoues)
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ vavavoum/Voiture.vb
@@ -0,0 +1,11 @@
+﻿Public Class Voiture
+    Private _roues As String = "4x195/55R16"
+
+    Public Property lesRoues As String
+        Get
+            Return _roues
+        End Get
+        Set(value As String)
+        End Set
+    End Property
+End Class
