d1ce616d116d69401528c6d89ec0c95a8ab6824c
Laurent Fréval <laurent.freval@outlook.com>
2019-12-11 09:12:36 +0100

    diapo 85

--- vavavoum/Module1.vb
+++ vavavoum/Module1.vb
@@ -4,14 +4,20 @@ Option Strict On
 Module Module1
     ReadOnly maClio As New Voiture()
     ReadOnly monTrajet As New Trajet()
-
+    ReadOnly listeDeCommutateurs As New List(Of String) From {"--help", "-help", "/?"}
 
     Sub Main()
 
         'maClio.lesRoues = "3x155/55R14"
-        Console.WriteLine(maClio)
-        Console.WriteLine(maClio.getMoteur)
-        Console.ReadKey()
+        'Console.WriteLine(maClio)
+        'Console.WriteLine(maClio.getMoteur)
+        'Console.ReadKey()
+        For Each argument As String In My.Application.CommandLineArgs
+            If listeDeCommutateurs.Contains(argument) Then
+                Aide()
+                Exit Sub
+            End If
+        Next
 
         monTrajet.Saisir()
         Console.WriteLine(monTrajet)
