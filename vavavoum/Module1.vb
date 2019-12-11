21e86c87f95fd9af6caa177f80be925ccfc05672
Laurent Fréval <laurent.freval@outlook.com>
2019-12-11 15:45:22 +0100

    module outils (avec montrajet public) diapo 101

--- vavavoum/Module1.vb
+++ vavavoum/Module1.vb
@@ -3,7 +3,7 @@ Option Strict On
 
 Module Module1
     ReadOnly maClio As New Voiture()
-    Dim monTrajet As New Trajet()
+    Public monTrajet As New Trajet()
     ReadOnly listeDeCommutateurs As New List(Of String) From {"--help", "-help", "/?"}
 
     Sub Main()
@@ -25,35 +25,5 @@ Module Module1
 
     End Sub
 
-    Public Function Saisir(prompt As String, type As String) As Object
-        Dim value As String
-        Console.Write(prompt & " ")
-        value = Console.ReadLine()
 
-        If type.Equals("String") Then
-            Return value
-        ElseIf type.Equals("Integer") And IsNumeric(value) Then
-            Return CInt(value)
-        Else
-            Return Nothing
-        End If
-    End Function
-    Public Sub Saisir()
-        Dim dep As String
-        Dim arr As String
-        Dim dist As Integer
-
-        Console.Write(My.Resources.Départ)
-        dep = Console.ReadLine()
-
-        Console.Write(My.Resources.Arrivée)
-        arr = Console.ReadLine()
-
-        dist = 300
-        monTrajet = New Trajet(dep, arr, dist)
-    End Sub
-
-    Sub Aide()
-        Console.WriteLine("ce programme permet d'enregistrer un trajet effectué en voiture")
-    End Sub
 End Module
---
+++ vavavoum/Outils.vb
@@ -0,0 +1,34 @@
+﻿Module Outils
+
+    Public Function Saisir(prompt As String, type As String) As Object
+        Dim value As String
+        Console.Write(prompt & " ")
+        value = Console.ReadLine()
+
+        If type.Equals("String", System.StringComparison.CurrentCulture) Then
+            Return value
+        ElseIf type.Equals("Integer", System.StringComparison.CurrentCulture) And IsNumeric(value) Then
+            Return CInt(value)
+        Else
+            Return Nothing
+        End If
+    End Function
+    Public Sub Saisir()
+        Dim dep As String
+        Dim arr As String
+        Dim dist As Integer
+
+        Console.Write(My.Resources.Départ & " ")
+        dep = Console.ReadLine()
+
+        Console.Write(My.Resources.Arrivée & " ")
+        arr = Console.ReadLine()
+
+        dist = 300
+        monTrajet = New Trajet(dep, arr, dist)
+    End Sub
+
+    Sub Aide()
+        Console.WriteLine("ce programme permet d'enregistrer un trajet effectué en voiture")
+    End Sub
+End Module
